3bb727102f161f6a87bdae41156a9e53327bf9ad
Koala Yeung <koalay@gmail.com>
2018-12-20 13:59:21 +0800

    Bash refactor (#3)
    * Rewritten xgettext script
    
    * proper help messages when run with "-h".
    
    * use loop to reduce copying code.
    
    * allow overriding the Gibbon source directory or
      the I18N directory by command options.
    
    * log errors in log file.
    
    * skip checking format with msgfmt (because
      not all translation strings are printf format
      string)
    
    * travis: run xgettextGenerationCommands.sh for test
    
    * Test if xgettextGenerationCommands.sh can be run successfully.
    
    * Ignore system temp files

--- xgettextGenerationCommands.sh
+++ xgettextGenerationCommands.sh
@@ -1,118 +1,190 @@
-cd /Applications/MAMP/htdocs/github_gibbonEdu/core/
-echo 'en_GB'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/en_GB/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/en_GB/LC_MESSAGES/gibbon.mo ./i18n/en_GB/LC_MESSAGES/gibbon.po
-echo 'zh_CN'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/zh_CN/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/zh_CN/LC_MESSAGES/gibbon.mo ./i18n/zh_CN/LC_MESSAGES/gibbon.po
-echo 'zh_HK'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/zh_HK/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/zh_HK/LC_MESSAGES/gibbon.mo ./i18n/zh_HK/LC_MESSAGES/gibbon.po
-echo 'en_US'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/en_US/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/en_US/LC_MESSAGES/gibbon.mo ./i18n/en_US/LC_MESSAGES/gibbon.po
-echo 'pl_PL'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/pl_PL/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/pl_PL/LC_MESSAGES/gibbon.mo ./i18n/pl_PL/LC_MESSAGES/gibbon.po
-echo 'it_IT'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/it_IT/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/it_IT/LC_MESSAGES/gibbon.mo ./i18n/it_IT/LC_MESSAGES/gibbon.po
-echo 'es_ES'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/es_ES/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/es_ES/LC_MESSAGES/gibbon.mo ./i18n/es_ES/LC_MESSAGES/gibbon.po
-echo 'id_ID'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/id_ID/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/id_ID/LC_MESSAGES/gibbon.mo ./i18n/id_ID/LC_MESSAGES/gibbon.po
-echo 'ar_SA'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ar_SA/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ar_SA/LC_MESSAGES/gibbon.mo ./i18n/ar_SA/LC_MESSAGES/gibbon.po
-echo 'fr_FR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/fr_FR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/fr_FR/LC_MESSAGES/gibbon.mo ./i18n/fr_FR/LC_MESSAGES/gibbon.po
-echo 'sw_KE'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/sw_KE/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/sw_KE/LC_MESSAGES/gibbon.mo ./i18n/sw_KE/LC_MESSAGES/gibbon.po
-echo 'pt_PT'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/pt_PT/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/pt_PT/LC_MESSAGES/gibbon.mo ./i18n/pt_PT/LC_MESSAGES/gibbon.po
-echo 'ro_RO'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ro_RO/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ro_RO/LC_MESSAGES/gibbon.mo ./i18n/ro_RO/LC_MESSAGES/gibbon.po
-echo 'ur_IN'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ur_IN/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ur_IN/LC_MESSAGES/gibbon.mo ./i18n/ur_IN/LC_MESSAGES/gibbon.po
-echo 'ja_JP'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ja_JP/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ja_JP/LC_MESSAGES/gibbon.mo ./i18n/ja_JP/LC_MESSAGES/gibbon.po
-echo 'ru_RU'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ru_RU/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ru_RU/LC_MESSAGES/gibbon.mo ./i18n/ru_RU/LC_MESSAGES/gibbon.po
-echo 'uk_UA'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/uk_UA/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/uk_UA/LC_MESSAGES/gibbon.mo ./i18n/uk_UA/LC_MESSAGES/gibbon.po
-echo 'bn_BD'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/bn_BD/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/bn_BD/LC_MESSAGES/gibbon.mo ./i18n/bn_BD/LC_MESSAGES/gibbon.po
-echo 'da_DK'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/da_DK/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/da_DK/LC_MESSAGES/gibbon.mo ./i18n/da_DK/LC_MESSAGES/gibbon.po
-echo 'fa_IR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/fa_IR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/fa_IR/LC_MESSAGES/gibbon.mo ./i18n/fa_IR/LC_MESSAGES/gibbon.po
-echo 'pt_BR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/pt_BR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/pt_BR/LC_MESSAGES/gibbon.mo ./i18n/pt_BR/LC_MESSAGES/gibbon.po
-echo 'nl_NL'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/nl_NL/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/nl_NL/LC_MESSAGES/gibbon.mo ./i18n/nl_NL/LC_MESSAGES/gibbon.po
-echo 'ka_GE'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ka_GE/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ka_GE/LC_MESSAGES/gibbon.mo ./i18n/ka_GE/LC_MESSAGES/gibbon.po
-echo 'hu_HU'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/hu_HU/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/hu_HU/LC_MESSAGES/gibbon.mo ./i18n/hu_HU/LC_MESSAGES/gibbon.po
-echo 'bg_BG'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/bg_BG/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/bg_BG/LC_MESSAGES/gibbon.mo ./i18n/bg_BG/LC_MESSAGES/gibbon.po
-echo 'ko_KP'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/ko_KP/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/ko_KP/LC_MESSAGES/gibbon.mo ./i18n/ko_KP/LC_MESSAGES/gibbon.po
-echo 'fi_FI'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/fi_FI/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/fi_FI/LC_MESSAGES/gibbon.mo ./i18n/fi_FI/LC_MESSAGES/gibbon.po
-echo 'de_DE'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/de_DE/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/de_DE/LC_MESSAGES/gibbon.mo ./i18n/de_DE/LC_MESSAGES/gibbon.po
-echo 'in_OR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/in_OR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/in_OR/LC_MESSAGES/gibbon.mo ./i18n/in_OR/LC_MESSAGES/gibbon.po
-echo 'no_NO'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/no_NO/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/no_NO/LC_MESSAGES/gibbon.mo ./i18n/no_NO/LC_MESSAGES/gibbon.po
-echo 'vi_VN'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/vi_VN/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/vi_VN/LC_MESSAGES/gibbon.mo ./i18n/vi_VN/LC_MESSAGES/gibbon.po
-echo 'sq_AL'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/sq_AL/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/sq_AL/LC_MESSAGES/gibbon.mo ./i18n/sq_AL/LC_MESSAGES/gibbon.po
-echo 'th_TH'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/th_TH/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/th_TH/LC_MESSAGES/gibbon.mo ./i18n/th_TH/LC_MESSAGES/gibbon.po
-echo 'el_GR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/el_GR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/el_GR/LC_MESSAGES/gibbon.mo ./i18n/el_GR/LC_MESSAGES/gibbon.po
-echo 'am_ET'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/am_ET/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/am_ET/LC_MESSAGES/gibbon.mo ./i18n/am_ET/LC_MESSAGES/gibbon.po
-echo 'om_ET'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/om_ET/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/om_ET/LC_MESSAGES/gibbon.mo ./i18n/om_ET/LC_MESSAGES/gibbon.po
-echo 'hr_HR'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/hr_HR/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/hr_HR/LC_MESSAGES/gibbon.mo ./i18n/hr_HR/LC_MESSAGES/gibbon.po
-echo 'et_EE'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/et_EE/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/et_EE/LC_MESSAGES/gibbon.mo ./i18n/et_EE/LC_MESSAGES/gibbon.po
-echo 'he_IL'
-xgettext --from-code=iso-8859-1 --omit-header -j --language=PHP --keyword=__:1,1t --keyword=__:2,2t -o ./i18n/he_IL/LC_MESSAGES/gibbon.po $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" | sed 's/ /*/g')
-msgfmt -cv -o ./i18n/he_IL/LC_MESSAGES/gibbon.mo ./i18n/he_IL/LC_MESSAGES/gibbon.po
+#!/bin/bash
+
+# ensure that realpath exists in the environment
+# (compatibility with macos)
+if ! which realpath 2>&1 >/dev/null; then
+    # declare a bash function which functions as
+    # normal realpath in bash
+    function realpath() {
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+
+# Print usage messages. First argument being the name of
+# the command.
+function usage() {
+    echo "Usage: $1 [OPTIONS]"
+    echo "Extracts translatable text from GibbonEdu source code to generate the"
+    echo "i18n folder containing mo/po files."
+    echo
+    echo "Options:"
+    echo
+    echo "-h            Displaying this message."
+    echo
+    echo "-l <locale>   Locale to process. If not specified, all existing locale"
+    echo "              will be processed."
+    echo
+    echo "-o <folder>   Setting output folder. Supposedly the folder(s) to output"
+    echo "              the mo/po files."
+    echo "              (Default: $I18N_HOME)"
+    echo
+    echo "-s <folder>   Source code folder of GibbonEdu/core."
+    echo "              (Default: $GIBBON_HOME)"
+    echo
+}
+
+# declare locale code in the array
+# in alphbetical order.
+declare -a LOCALES=(
+    "am_ET"
+    "ar_SA"
+    "bg_BG"
+    "bn_BD"
+    "da_DK"
+    "de_DE"
+    "el_GR"
+    "en_GB"
+    "en_US"
+    "es_ES"
+    "et_EE"
+    "fa_IR"
+    "fi_FI"
+    "fr_FR"
+    "he_IL"
+    "hr_HR"
+    "hu_HU"
+    "id_ID"
+    "in_OR"
+    "it_IT"
+    "ja_JP"
+    "ka_GE"
+    "ko_KP"
+    "nl_NL"
+    "no_NO"
+    "om_ET"
+    "pl_PL"
+    "pt_BR"
+    "pt_PT"
+    "ro_RO"
+    "ru_RU"
+    "sq_AL"
+    "sw_KE"
+    "th_TH"
+    "uk_UA"
+    "ur_IN"
+    "vi_VN"
+    "zh_CN"
+    "zh_HK"
+)
+
+# default variables
+GIBBON_HOME="/Applications/MAMP/htdocs/github_gibbonEdu/core/"
+I18N_HOME=$(dirname $(realpath $0))
+COLS=$(tput cols)
+LOGFILE=$PWD/$(basename -s .sh $0).log
+
+if [ ! -d "$GIBBON_HOME" ]; then
+    # if the system do not have that default home directory,
+    # use the parent folder of this source code repository
+    # as the Gibbon source code home.
+    GIBBON_HOME=$(dirname $(dirname $(realpath $0)))
+fi
+
+# get options to override default variables
+while getopts "hl:o:s:" ARG; do
+    case $ARG in
+        h)
+            usage $0
+            exit 0
+            ;;
+        l)
+            LOCALE="$OPTARG"
+            ;;
+        o)
+            I18N_HOME=$(realpath "$OPTARG")
+            ;;
+        s)
+            GIBBON_HOME=$(realpath "$OPTARG")
+            ;;
+        *)
+            usage $0
+            exit 1
+            ;;
+    esac
+done
+
+if [ ! -d "$GIBBON_HOME" ]; then
+    echo "\"$GIBBON_HOME\" is not a valid directory."
+    exit 1
+fi
+if [ ! -d "$I18N_HOME" ]; then
+    echo "\"$I18N_HOME\" is not a valid directory."
+    exit 1
+fi
+
+# remove trailing slash from I18N_HOME
+I18N_HOME=${I18N_HOME%/}
+
+# go to the Gibbon installation home folder
+OLD_PWD=$PWD
+cd $GIBBON_HOME
+
+# generate the locale
+if [ "$LOCALE" != "" ]; then
+    if [ ! -d "$I18N_HOME/$LOCALE" ]; then
+        echo "\"$I18N_HOME/$LOCALE\" is not a valid directory."
+        exit 1
+    fi
+    declare -a LOCALES=($LOCALE)
+fi
+
+# loop through all the specified locale to run
+# text extractions and compile catalog to binary
+# format.
+echo -en "" > $LOGFILE
+FAIL=0
+for LOCALE in "${LOCALES[@]}"
+do
+    echo -e "\nlocale: $LOCALE\n-------------"
+
+    echo -en "* generate locale text file (.po)\r"
+    xgettext \
+        --from-code=iso-8859-1 \
+        --omit-header -j \
+        --language=PHP \
+        --keyword=__:1,1t \
+        --keyword=__:2,2t \
+        -o "$I18N_HOME/$LOCALE/LC_MESSAGES/gibbon.po" \
+        $(find . -type f -name "*.php" ! -path "./lib/*" ! -path "./tests/*" ! -path "./vendor/*" ! -path "./.git/*" | sed 's/ /*/g') \
+        2>>$LOGFILE >/dev/null
+    if [ "$?" -eq 0 ]; then
+        echo -e  "* generate locale text file (.po)\t\t\e[32m✓\e[0m"
+    else
+        echo -e  "* generate locale text file (.po)\t\t\e[31m✘\e[0m"
+        FAIL=1
+    fi
+
+    echo -en "* generate locale binary file (.mo)\r"
+    msgfmt --check-header --check-domain -v \
+        -o "$I18N_HOME/$LOCALE/LC_MESSAGES/gibbon.mo" \
+        "$I18N_HOME/$LOCALE/LC_MESSAGES/gibbon.po" \
+        2>>$LOGFILE >/dev/null
+    if [ "$?" -eq 0 ]; then
+        echo -e  "* generate locale binary file (.mo)\t\t\e[32m✓\e[0m"
+    else
+        echo -e  "* generate locale binary file (.mo)\t\t\e[31m✘\e[0m"
+        FAIL=1
+    fi
+done
+
+# Show log, if needed
+if [ $FAIL -eq 1 ]; then
+    echo
+    echo Failed
+    echo ------
+    cat $LOGFILE
+    exit 1
+fi
+
+# return to original folder
+cd $OLD_PWD
